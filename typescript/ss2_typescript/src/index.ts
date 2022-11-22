5dd9f8f3628819eecb7721f7733b31171ec28761
Minchou99 <minhchaupt2903@gmail.com>
2022-11-22 10:59:15 +0700

    update

---
+++ typescript/ss2_typescript/src/index.ts
@@ -0,0 +1,17 @@
+// happy coding 👻
+function totalFibonacci(count: number) {
+    let num1 = 0;
+    let num2 = 1;
+    let total;
+    let fibonacci :Array<number> = [];
+    for(let i = 0; i < count; i++){
+        fibonacci.push(num1);
+        total = num1 + num2;
+        num1 = num2;
+        num2 = total;
+    }
+    console.log('fibonacci: '+ fibonacci );
+    console.log('total: ' + total);
+    return total;
+}
+totalFibonacci(15);
